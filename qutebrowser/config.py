1ae5a581848424ad199f396eda0295c70c4f39bd
Marco La Rosa <marco.larosa1201@gmail.com>
2025-10-30 21:04:45 +0100

    fixed keybinds and stuff for laptop

--- qutebrowser/config.py
+++ qutebrowser/config.py
@@ -275,7 +275,7 @@ c.url.searchengines = {
         'DEFAULT': 'https://duckduckgo.com/?q={}',
         '!aw': 'https://wiki.archlinux.org/?search={}',
         '!apkg': 'https://archlinux.org/packages/?sort=&q={}&maintainer=&flagged=',
-        '!gh': 'https://github.com/search?o=desc&q={}&s=stars',
+        '!git': 'https://github.com/search?o=desc&q={}&s=stars',
         '!yt': 'https://www.youtube.com/results?search_query={}',
         '!wk': 'https://it.wikipedia.org/w/index.php?search={}',
         '!gpt': 'https://chatgpt.com/?q={}',
